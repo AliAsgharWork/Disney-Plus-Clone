a4e648041e7163b31d79f260e994987e28f63754
Ali Asghar <56997518+AliAsgharWork@users.noreply.github.com>
2022-09-16 12:15:41 +0100

    Completed Application
    Committing after completion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
 import "./App.css";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-      <h2>About to build a disney + React application</h2>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
